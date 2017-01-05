218cbad5aeb7937437fb2d50e7a3c13cc4b1e5ee
gioGats <ryangiarusso@gmail.com>
2017-01-05 18:16:12 +1300

    Non-definite filling, first attempt

--- brute_force.py
+++ brute_force.py
@@ -2,6 +2,18 @@ import sys
 import time
 
 
+class ProgressStalled(Exception):
+    pass
+
+
+class Failure(Exception):
+    pass
+
+
+class Finished(Exception):
+    pass
+
+
 class Puzzle(object):
     def __init__(self, string=None):
         assert (isinstance(string, str) or isinstance(string, None))
@@ -10,44 +22,105 @@ class Puzzle(object):
         else:
             self.board = []
             for i in list(string):
-                self.board.append(Cell(int(i)))
+                self.board.append(Cell(list(string).index(i), int(i)))
             self.rows = self.make_rows()
             self.columns = self.make_columns()
             self.squares = self.make_squares()
 
-    def solve(self):
-        while not self.is_finished():
-            if not self.iterate():
-                break
-            # #print(self)
-
-    def iterate(self):
-        improve = 0
-        for i in [self.rows, self.columns, self.squares]:
-            for j in i:
-                current_actuals = []
-                for k in j:
-                    # noinspection PyTypeChecker
-                    cell = self.board[k]
-                    current_actuals.append(cell.actual)
-                for k in j:
-                    # noinspection PyTypeChecker
-                    cell = self.board[k]
-                    cell.remove_possible(current_actuals)
-                    # FUTURE deal with potential hang
-        for cell in self.board:
-            if cell.check():
-                improve += 1
-        if improve > 0:
-            return True
+    def solve(self, non_definite=False):
+        try:
+            self.definite_fill()
+        except ProgressStalled:
+            pass
+        except Finished:
+            return
+        if non_definite:
+            try:
+                self.non_definite_fill()
+            except ProgressStalled:
+                pass
+            except Finished:
+                return
+            except Failure:
+                pass
         else:
-            return False
+            raise Failure
+
+    def definite_fill(self):
+        while not self.is_finished():
+            improve = 0
+            for i in [self.rows, self.columns, self.squares]:
+                for j in i:
+                    current_actuals = []
+                    for k in j:
+                        # noinspection PyTypeChecker
+                        cell = self.board[k]
+                        current_actuals.append(cell.actual)
+                    for k in j:
+                        # noinspection PyTypeChecker
+                        cell = self.board[k]
+                        cell.remove_possible(current_actuals)
+            for cell in self.board:
+                if cell.check():
+                    improve += 1
+            if improve <= 0:
+                raise ProgressStalled
+        raise Finished
+
+    def non_definite_fill(self):
+        cells_remaining = []
+        for cell in self.board:
+            if cell.actual == 0:
+                cells_remaining.append(cell)
+        cells_remaining.sort()
+        options = []
+        for cell in cells_remaining:
+            for option in cell.possible:
+                options.append((cell.index, option))
+        while not self.is_finished():
+            choice = options.pop()
+            new_puzzle = Puzzle(self.__repr__())
+            new_puzzle.board[choice[0]].actual = choice[1]
+            try:
+                new_puzzle.solve(non_definite=False)
+            except ProgressStalled:
+                pass
+            except Finished:
+                self.board = new_puzzle.board
+                raise Finished
+
+        cells_remaining = []
+        for cell in self.board:
+            if cell.actual == 0:
+                cells_remaining.append(cell)
+        cells_remaining.sort()
+        options = []
+        for cell in cells_remaining:
+            for option in cell.possible:
+                options.append((cell.index, option))
+        while not self.is_finished():
+            choice = options.pop()
+            new_puzzle = Puzzle(self.__repr__())
+            new_puzzle.board[choice[0]].actual = choice[1]
+            try:
+                new_puzzle.solve(non_definite=True)
+            except ProgressStalled:
+                pass
+            except Finished:
+                self.board = new_puzzle.board
+                raise Finished
 
     def is_finished(self):
-        for i in self.board:
-            if i.actual is None or i.actual == 0:
+        for cell in self.board:
+            if cell.actual == 0:
                 return False
         return True
+
+    def failure(self):
+        for cell in self.board:
+            if cell.actual == 0 and len(cell.possible) == 0:
+                return True
+        return False
 
     @staticmethod
     def make_rows():
@@ -103,7 +176,8 @@ class Puzzle(object):
 
 
 class Cell(object):
-    def __init__(self, value=0):
+    def __init__(self, index, value=0):
+        self.index = index
         if value == 0:
             self.possible = [1, 2, 3, 4, 5, 6, 7, 8, 9]
             self.actual = 0
@@ -131,6 +205,12 @@ class Cell(object):
     def __eq__(self, other):
         return self.actual == other.actual
 
+    def __gt__(self, other):
+        return len(self.possible) > len(other.possible)
+
+    def __lt__(self, other):
+        return len(self.possible) < len(other.possible)
+
 
 if __name__ == '__main__':
     try:
@@ -140,27 +220,29 @@ if __name__ == '__main__':
     print('Testing brute force, %d examples' % num_examples)
     # FUTURE Add random sampling from complete csv
     examples = []
-
     for line in open('data/sudoku_copy.csv', 'r'):
         examples.append(line.replace('\n', '').split(","))
+    examples.reverse()
     global_start = time.time()
-    trial = 1
-    success = fail = 0
-    for ex in examples:
-        local_start = time.time()
-        start = ex[0]
-        solution = ex[1]
-        puzzle = Puzzle(start)
-        print("Trial %d of %d:" % (trial, num_examples), end='')
-        puzzle.solve()
-        local_end = time.time()
-        if puzzle == solution:
-            print(' success in %.10f seconds' % (local_end - local_start))
-            success += 1
-        else:
-            print(' failure in %.10f seconds' % (local_end - local_start))
-            fail += 1
-        trial += 1
-    global_end = time.time()
-    print('Testing complete in %.10f seconds' % (global_end - global_start))
-    print('%d suceesses and %d failures' % (success, fail))
+    trial = success = fail = 0
+    try:
+        for ex in examples:
+            local_start = time.time()
+            trial += 1
+            start = ex[0]
+            solution = ex[1]
+            puzzle = Puzzle(start)
+            print("\rTrial %d of %d:" % (trial, num_examples), end='')
+            puzzle.solve(non_definite=True)
+            local_end = time.time()
+            if puzzle == solution:
+                # #print(' success in %.10f seconds' % (local_end - local_start))
+                success += 1
+            else:
+                # #print(' failure in %.10f seconds' % (local_end - local_start))
+                fail += 1
+        raise KeyboardInterrupt
+    except KeyboardInterrupt:
+        global_end = time.time()
+        print('Testing complete in %.10f seconds' % (global_end - global_start))
+        print('%d suceesses and %d failures in %d trials' % (success, fail, trial))
