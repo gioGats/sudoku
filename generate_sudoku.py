238c78398e178cf38ad7f6174cdac3be3666d809
keithc <keithaf98@gmail.com>
2017-01-31 18:04:22 -0600

    First commit, basic api code for generate_sudoku.py

--- generate_sudoku.py
+++ generate_sudoku.py
@@ -1,20 +1,26 @@
-#!/usr/bin/python2
 """
-This is adapted from https://www.ocf.berkeley.edu/~arel/sudoku/main.html.
-Generates 1 million Sudoku games. 
-Kyubyong Park. kbpark.linguist@gmail.com www.github.com/kyubyong
+Code Sources Used:
+https://gist.github.com/mythz/5723202
+https://github.com/dartist/sudoku_solver/blob/master/benchmarks/sudoku.py
+https://github.com/gioGats/sudoku
+http://norvig.com/sudoku.html
 """
+import random
+
+
+# API/Adapted Code #####################################################################################################
 
 def generate_puzzles(num_puzzles, num_clues, output_filename):
     # TODO Generate num_puzzles with num_clues
     # TODO verify this works
     output = open(output_filename, 'w')
     for i in range(num_puzzles):
-        output.write(generate_puzzle(num_clues, string=True))
+        output.write(generate_puzzle(num_clues, asTuple=False))
         output.write('\n')
     output.close()
 
-def generate_puzzle(num_clues, string=True, tuple=False):
+
+def generate_puzzle(num_clues, asTuple=False):
     # TODO Generate a puzzle, solution tuple where the puzzle has num_clues
     # TODO if string, return as a string, example:
     #    '004300209005009001070060043006002087190007400050083000600000105003508690042910300,
@@ -26,187 +32,155 @@ def generate_puzzle(num_clues, string=True, tuple=False):
     # TODO Must be a valid puzzle!
     # TODO Use of the below refence code is fine, but make sure it works in Python3 (it's currently in Python2)
     # TODO Make sure it works at the theoretical limit (num_clues -> 17)
-    return ''
-    
+    result = random_puzzle(num_clues)
+    solution = solve(result)
+    solution_string, solution_tuple = "", []
+
+    for r in rows:
+        solution_string += (''.join(solution[r + c] for c in cols))
+
+    if asTuple:
+        solution_tuple.extend((result, solution_string))
+        return solution_tuple
+    else:
+        return result + ',\n' + solution_string
 
 
-import random, copy
-import numpy as np
+# Norvig's Code ########################################################################################################
 
-sample  = [ [3,4,1,2,9,7,6,8,5],
-            [2,5,6,8,3,4,9,7,1],
-            [9,8,7,1,5,6,3,2,4],
-            [1,9,2,6,7,5,8,4,3],
-            [8,7,5,4,2,3,1,9,6],
-            [6,3,4,9,1,8,2,5,7],
-            [5,6,3,7,8,9,4,1,2],
-            [4,1,9,5,6,2,7,3,8],
-            [7,2,8,3,4,1,5,6,9] ]
-            
-"""
-Randomly arrange numbers in a grid while making all rows, columns and
-squares (sub-grids) contain the numbers 1 through 9.
+def cross(A, B):
+    """Cross product of elements in A and elements in B."""
+    return [a + b for a in A for b in B]
 
-For example, "sample" (above) could be the output of this function. """
-def construct_puzzle_solution():
-    # Loop until we're able to fill all 81 cells with numbers, while
-    # satisfying the constraints above.
-    while True:
-        try:
-            puzzle  = [[0]*9 for i in range(9)] # start with blank puzzle
-            rows    = [set(range(1,10)) for i in range(9)] # set of available
-            columns = [set(range(1,10)) for i in range(9)] #   numbers for each
-            squares = [set(range(1,10)) for i in range(9)] #   row, column and square
-            for i in range(9):
-                for j in range(9):
-                    # pick a number for cell (i,j) from the set of remaining available numbers
-                    choices = rows[i].intersection(columns[j]).intersection(squares[(i/3)*3 + j/3])
-                    choice  = random.choice(list(choices))
-        
-                    puzzle[i][j] = choice
-        
-                    rows[i].discard(choice)
-                    columns[j].discard(choice)
-                    squares[(i/3)*3 + j/3].discard(choice)
 
-            # success! every cell is filled.
-            return puzzle
-            
-        except IndexError:
-            # if there is an IndexError, we have worked ourselves in a corner (we just start over)
-            pass
+digits = '123456789'
+rows = 'ABCDEFGHI'
+cols = digits
+squares = cross(rows, cols)
+unitlist = ([cross(rows, c) for c in cols] +
+            [cross(r, cols) for r in rows] +
+            [cross(rs, cs) for rs in ('ABC', 'DEF', 'GHI') for cs in ('123', '456', '789')])
+units = dict((s, [u for u in unitlist if s in u])
+             for s in squares)
+peers = dict((s, set(sum(units[s], [])) - {s})
+             for s in squares)
 
-"""
-Randomly pluck out cells (numbers) from the solved puzzle grid, ensuring that any
-plucked number can still be deduced from the remaining cells.
 
-For deduction to be possible, each other cell in the plucked number's row, column,
-or square must not be able to contain that number. """
-def pluck(puzzle, n=0):
+# Parse a Grid #########################################################################################################
 
-    """
-    Answers the question: can the cell (i,j) in the puzzle "puz" contain the number
-    in cell "c"? """
-    def canBeA(puz, i, j, c):
-        v = puz[c/9][c%9]
-        if puz[i][j] == v: return True
-        if puz[i][j] in range(1,10): return False
-            
-        for m in range(9): # test row, col, square
-            # if not the cell itself, and the mth cell of the group contains the value v, then "no"
-            if not (m==c/9 and j==c%9) and puz[m][j] == v: return False
-            if not (i==c/9 and m==c%9) and puz[i][m] == v: return False
-            if not ((i/3)*3 + m/3==c/9 and (j/3)*3 + m%3==c%9) and puz[(i/3)*3 + m/3][(j/3)*3 + m%3] == v:
+def parse_grid(grid):
+    """Convert grid to a dict of possible values, {square: digits}, or
+    return False if a contradiction is detected."""
+    # To start, every square can be any digit; then assign values from the grid.
+    values = dict((s, digits) for s in squares)
+    for s, d in grid_values(grid).items():
+        if d in digits and not assign(values, s, d):
+            return False  # (Fail if we can't assign d to square s.)
+    return values
+
+
+def grid_values(grid):
+    """Convert grid into a dict of {square: char} with '0' or '.' for empties."""
+    chars = [c for c in grid if c in digits or c in '0.']
+    assert len(chars) == 81
+    return dict(zip(squares, chars))
+
+
+# Constraint Propagation ###############################################################################################
+
+def assign(values, s, d):
+    """Eliminate all the other values (except d) from values[s] and propagate.
+    Return values, except return False if a contradiction is detected."""
+    other_values = values[s].replace(d, '')
+    if all(eliminate(values, s, d2) for d2 in other_values):
+        return values
+    else:
+        return False
+
+
+def eliminate(values, s, d):
+    """Eliminate d from values[s]; propagate when values or places <= 2.
+    Return values, except return False if a contradiction is detected."""
+    if d not in values[s]:
+        return values  # Already eliminated
+    values[s] = values[s].replace(d, '')
+    # (1) If a square s is reduced to one value d2, then eliminate d2 from the peers.
+    if len(values[s]) == 0:
+        return False  # Contradiction: removed last value
+    elif len(values[s]) == 1:
+        d2 = values[s]
+        if not all(eliminate(values, s2, d2) for s2 in peers[s]):
+            return False
+    # (2) If a unit u is reduced to only one place for a value d, then put it there.
+    for u in units[s]:
+        dplaces = [s for s in u if d in values[s]]
+        if len(dplaces) == 0:
+            return False  # Contradiction: no place for this value
+        elif len(dplaces) == 1:
+            # d can only be in one place in unit; assign it there
+            if not assign(values, dplaces[0], d):
                 return False
-
-        return True
-
-
-    """
-    starts with a set of all 81 cells, and tries to remove one (randomly) at a time
-    but not before checking that the cell can still be deduced from the remaining cells. """
-    cells     = set(range(81))
-    cellsleft = cells.copy()
-    while len(cells) > n and len(cellsleft):
-        cell = random.choice(list(cellsleft)) # choose a cell from ones we haven't tried
-        cellsleft.discard(cell) # record that we are trying this cell
-
-        # row, col and square record whether another cell in those groups could also take
-        # on the value we are trying to pluck. (If another cell can, then we can't use the
-        # group to deduce this value.) If all three groups are True, then we cannot pluck
-        # this cell and must try another one.
-        row = col = square = False
-
-        for i in range(9):
-            if i != cell/9:
-                if canBeA(puzzle, i, cell%9, cell): row = True
-            if i != cell%9:
-                if canBeA(puzzle, cell/9, i, cell): col = True
-            if not (((cell/9)/3)*3 + i/3 == cell/9 and ((cell/9)%3)*3 + i%3 == cell%9):
-                if canBeA(puzzle, ((cell/9)/3)*3 + i/3, ((cell/9)%3)*3 + i%3, cell): square = True
-
-        if row and col and square:
-            continue # could not pluck this cell, try again.
-        else:
-            # this is a pluckable cell!
-            puzzle[cell/9][cell%9] = 0 # 0 denotes a blank cell
-            cells.discard(cell) # remove from the set of visible cells (pluck it)
-            # we don't need to reset "cellsleft" because if a cell was not pluckable
-            # earlier, then it will still not be pluckable now (with less information
-            # on the board).
-
-    # This is the puzzle we found, in all its glory.
-    return (puzzle, len(cells))
-    
-    
-"""
-That's it.
-
-If we want to make a puzzle we can do this:
-    pluck(construct_puzzle_solution())
-    
-The following functions are convenience functions for doing just that...
-"""
+    return values
 
 
+# Display as 2-D grid ##################################################################################################
 
-"""
-This uses the above functions to create a new puzzle. It attempts to
-create one with 28 (by default) given cells, but if it can't, it returns
-one with as few givens as it is able to find.
-
-This function actually tries making 100 puzzles (by default) and returns
-all of them. The "best" function that follows this one selects the best
-one of those.
-"""
-def run(n = 28, iter=100):
-    all_results = {}
-#     print "Constructing a sudoku puzzle."
-#     print "* creating the solution..."
-    a_puzzle_solution = construct_puzzle_solution()
-    
-#     print "* constructing a puzzle..."
-    for i in range(iter):
-        puzzle = copy.deepcopy(a_puzzle_solution)
-        (result, number_of_cells) = pluck(puzzle, n)
-        all_results.setdefault(number_of_cells, []).append(result)
-        if number_of_cells <= n: break
- 
-    return all_results, a_puzzle_solution
-
-def best(set_of_puzzles):
-    # Could run some evaluation function here. For now just pick
-    # the one with the fewest "givens".
-    return set_of_puzzles[min(set_of_puzzles.keys())][0]
-
-def display(puzzle):
-    for row in puzzle:
-        print ' '.join([str(n or '_') for n in row])
-
-    
-# """ Controls starts here """
-# results = run(n=0)       # find puzzles with as few givens as possible.
-# puzzle  = best(results)  # use the best one of those puzzles.
-# display(puzzle)          # display that puzzle.
+def display(values):
+    """Display these values as a 2-D grid."""
+    width = 1 + max(len(values[s]) for s in squares)
+    line = '+'.join(['-' * (width * 3)] * 3)
+    for r in rows:
+        print(''.join(values[r + c].center(width) + ('|' if c in '36' else '')
+                      for c in cols))
+        if r in 'CF':
+            print(line)
 
 
-def main(num):
-    '''
-    Generates `num` games of Sudoku.
-    '''
-    quizzes = np.zeros((num, 9, 9), np.int32)
-    solutions = np.zeros((num, 9, 9), np.int32)
-    for i in range(num):
-        all_results, solution = run(n=23, iter=10)
-        quiz = best(all_results)
-        
-        quizzes[i] = quiz
-        solutions[i] = solution
+# Search ###############################################################################################################
 
-        if (i+1) % 1000 == 0:
-            print i+1
-            np.save('data/sudoku.npz', quizzes=quizzes, solutions=solutions)
+def solve(grid): return search(parse_grid(grid))
 
-if __name__ == "__main__":
-    main(1000000)
-    print "Done!"
+
+def search(values):
+    """Using depth-first search and propagation, try all possible values."""
+    if values is False:
+        return False  # Failed earlier
+    if all(len(values[s]) == 1 for s in squares):
+        return values  # Solved!
+    # Chose the unfilled square s with the fewest possibilities
+    n, s = min((len(values[s]), s) for s in squares if len(values[s]) > 1)
+    return some(search(assign(values.copy(), s, d))
+                for d in values[s])
+
+
+def some(seq):
+    """Return some element of seq that is true."""
+    for e in seq:
+        if e: return e
+    return False
+
+
+# API Used #############################################################################################################
+
+def random_puzzle(N=17):
+    """Make a random puzzle with N or more assignments. Restart on contradictions.
+    Note the resulting puzzle is not guaranteed to be solvable, but empirically
+    about 99.8% of them are solvable. Some have multiple solutions."""
+    values = dict((s, digits) for s in squares)
+    for s in shuffled(squares):
+        if not assign(values, s, random.choice(values[s])):
+            break
+        ds = [values[s] for s in squares if len(values[s]) == 1]
+        if len(ds) >= N and len(set(ds)) >= 8:
+            return ''.join(values[s] if len(values[s]) == 1 else '0' for s in squares)
+    return random_puzzle(N)  # Give up and make a new puzzle
+
+
+def shuffled(seq):
+    "Return a randomly shuffled copy of the input sequence."
+    seq = list(seq)
+    random.shuffle(seq)
+    return seq
+
+
+generate_puzzles(5, 17, 'sus.txt')
