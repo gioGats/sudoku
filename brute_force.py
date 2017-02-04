7b2bd28aef7ad0781ea5d27f022fb9378cb79060
keithc <keithaf98@gmail.com>
2017-02-03 20:09:39 -0600

    ERROR: Value assignment currently does not retain, DO  NOT USE THIS NORMALLY

--- brute_force.py
+++ brute_force.py
@@ -20,105 +20,70 @@ class Puzzle(object):
         if string is None:
             raise NotImplementedError("Random generation not enabled")
         else:
-            self.input_values = gs.grid_values(string)
-
-            self.TEMP_input_values = gs.parse_grid(string)
-
-            self.cell_values = list(self.input_values.keys())
-
-            self.editable_values = []
-            self.zeroes = 0
-
-            #gs.display(self.input_values)
-            #print('///////////////////')
-            #gs.display(self.TEMP_input_values)
-            #print('///////////////////')
-
-            # print(gs.units['A1'])
-            # print(gs.peers['A1'])
-
-    '''
-    first (P): generate a first candidate solution for P.
-    next (P, c): generate the next candidate for P after the current one c.
-    valid (P, c): check whether candidate c is a solution for P.
-    output (P, c): use the solution c of P as appropriate to the application.
-
-    c ← first(P)
-    while c ≠ Λ do
-     if valid(P,c) then output(P, c)
-     c ← next(P,c)
-    end while
-    '''
+            self.cell_values = gs.grid_values(string)
+            self.digits = '123456789'
+            self.rows = 'ABCDEFGHI'
+            self.cols = self.digits
+            self.puzzle_string = ''
 
     def solve(self):
-        # while '0' in (self.input_values[all_values] for all_values in self.cell_values):
+        editable_ids = []
         for cell_id in self.cell_values:
-            if self.input_values[cell_id] is '0':
-                self.zeroes += 1
-                self.editable_values.append(cell_id)
+            if self.cell_values[cell_id] is '0':
+                editable_ids.append(cell_id)
 
-        brute_dict = self.get_possible_values()
-
-        while self.zeroes > 14:
-            for i in range(0, len(self.editable_values), 1):
-                if self.help_brute_fill(self.editable_values[i], brute_dict[self.editable_values[i]]) is 'Set':
-                    self.zeroes -= 1
-                    # print(self.zeroes)
-                if not self.zeroes > 14:
-                    break
-
-        brute_dict = self.get_possible_values()
-
+        brute_dict = self.get_possible_values(editable_ids)
         brute_list = []
         brute_list_ids = []
-        for i in range(0, len(self.editable_values), 1):
-            brute_list_ids.append(str(self.editable_values[i]))
-            brute_list.append(list(brute_dict[str(self.editable_values[i])]))
+        for value in brute_dict.keys():
+            brute_list_ids.append(str(value))
+            brute_list.append(list(brute_dict[str(value)]))
 
-        # print(brute_list_ids)
-        # print(brute_list)
+        print(brute_list_ids)
+        print(brute_list)
 
-        for i in range(0, 1000000000, 10000):
-            if self.brute_gen(i, brute_list, brute_list_ids) is 'Done':
-                digits = '123456789'
-                rows = 'ABCDEFGHI'
-                cols = digits
-                puzzle_string = ''
-                for r in rows:
-                    puzzle_string += (''.join(self.input_values[r + c] for c in cols))
-                return puzzle_string
+        if self.brute_gen(brute_list, brute_list_ids):
+            results = self.cell_values
+            for r in self.rows:
+                self.puzzle_string += (''.join(results[r + c] for c in self.cols))
+            gs.display(self.cell_values)
+            print('///////////////////')
+            return self.puzzle_string
 
-        print('Failed.')
+        else:
+            gs.display(self.cell_values)
+            print('///////////////////')
+            print('Failed.')
 
-    def get_possible_values(self):
-        brute_dict = {}
-        for i in range(0, len(self.editable_values), 1):
+    def get_possible_values(self, values):
+        values_dict = {}
+        for i in range(0, len(values), 1):
             possible_values = set()
-            for value in list(self.valid_fill(self.editable_values[i])):
+            for value in list(self.valid_fill(values[i])):
                 possible_values.add(str(value))
             else:
-                brute_dict[str(self.editable_values[i])] = possible_values
-        return brute_dict
+                values_dict[str(values[i])] = possible_values
+        return values_dict
 
-    def brute_gen(self, start, brute_list, brute_list_ids):
-        brute_gen1000 = list(itertools.islice(itertools.product(*brute_list), start, start + 10000))
-
+    def brute_gen(self, brute_list, brute_list_ids):
+        brute_gen1000 = list(itertools.product(*brute_list))
         for sequence in brute_gen1000:
-            valid_result = self.brute_fill(brute_list_ids, sequence)
-            if valid_result is 'Valid':
-                return 'Done'
-            if valid_result is 'Invalid':
-                for cell_id in self.editable_values:
-                    self.input_values[cell_id] = 0
+            valid_result = self.brute_fill(brute_list_ids, list(sequence))
+            if valid_result:
+                return True
+            else:
+                for rollback_id in brute_list_ids:
+                    self.cell_values[rollback_id] = '0'
 
 # Keith's WIP Zone: UNDER CONSTRUCTION #################################################################################
 
+    '''
     def help_brute_fill(self, cell_id, possible_values):
         possible_values = list(possible_values)
         for i in range(0, len(possible_values), 1):
             if self.is_valid(cell_id, possible_values[i]):
-                self.input_values[cell_id] = possible_values[i]
-                return 'Set'
+                self.self.cell_values[cell_id] = possible_values[i]
+                return 'Set' '''
 
     def valid_fill(self, cell_id):
         possible_numbers = list(range(1, 10))
@@ -126,26 +91,28 @@ class Puzzle(object):
         # noinspection PyUnusedLocal
         for n in list(range(3)):
             for number in possible_numbers:
-                if str(number) in (self.input_values[cell_id2] for cell_id2 in gs.units[cell_id][0]):
+                if str(number) in (self.cell_values[cell_id2] for cell_id2 in gs.units[cell_id][0]):
                     possible_numbers.remove(number)
-                elif str(number) in (self.input_values[cell_id2] for cell_id2 in gs.units[cell_id][1]):
+                elif str(number) in (self.cell_values[cell_id2] for cell_id2 in gs.units[cell_id][1]):
                     possible_numbers.remove(number)
-                elif str(number) in (self.input_values[cell_id2] for cell_id2 in gs.units[cell_id][2]):
+                elif str(number) in (self.cell_values[cell_id2] for cell_id2 in gs.units[cell_id][2]):
                     possible_numbers.remove(number)
         return ''.join(map(str, possible_numbers))
 
-    def brute_fill(self, cell_ids, numbers):
+    def brute_fill(self, cell_ids, cell_values):
         for i in range(0, len(cell_ids), 1):
-            if self.is_valid(cell_ids[i], numbers[i]):
-                self.input_values[cell_ids[i]] = numbers[i]
-            else:
-                return 'Invalid'
-        return 'Valid'
+            self.cell_values[cell_ids[i]] = str(cell_values[i])
 
-    def is_valid(self, cell_id, number):
-        if str(number) not in (self.input_values[cell_id2] for cell_id2 in gs.units[cell_id][0]):
-            if str(number) not in (self.input_values[cell_id2] for cell_id2 in gs.units[cell_id][1]):
-                if str(number) not in (self.input_values[cell_id2] for cell_id2 in gs.units[cell_id][2]):
+        for i in range(0, len(cell_ids), 1):
+            if self.is_valid(cell_ids[i], cell_values[i]):
+                return True
+            else:
+                return False
+
+    def is_valid(self, cell_id, cell_value):
+        if str(cell_value) not in (self.cell_values[cell_id2] for cell_id2 in gs.units[cell_id][0]):
+            if str(cell_value) not in (self.cell_values[cell_id2] for cell_id2 in gs.units[cell_id][1]):
+                if str(cell_value) not in (self.cell_values[cell_id2] for cell_id2 in gs.units[cell_id][2]):
                     return True
         return False
 
@@ -159,9 +126,9 @@ if __name__ == '__main__':
     except IndexError:
         num_examples = 100  # Default
 
-    #Effective Range: 17-77
+    # Effective Range: 17-77
     print('Generating...')
-    gs.generate_puzzles(10, 60, 'data/sudoku.txt')
+    # gs.generate_puzzles(1, 77, 'data/sudoku.txt')
 
     print('Testing brute force, %d examples' % num_examples)
     # FUTURE Add random sampling from complete csv
@@ -179,7 +146,7 @@ if __name__ == '__main__':
             start = ex[0]
             solution = ex[1]
             puzzle = Puzzle(start)
-            print("\rTrial %d of %d:" % (trial, num_examples), end='')
+            print("Trial %d of %d:\n" % (trial, num_examples))
             puzzle = puzzle.solve()
             local_end = time.time()
             if puzzle == solution:
